17659deae695636cf70b7eb866ee88b585fda0a3
Noel <14931745+FireRedNinja@users.noreply.github.com>
2022-01-29 15:05:20 +0000

    tooltip (#12)

--- src/components/NarrativeText/NarrativeText.tsx
+++ src/components/NarrativeText/NarrativeText.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactMarkdown from "react-markdown";
 import "./NarrativeText.scss";
+import Tooltip from "rc-tooltip";
+import "rc-tooltip/assets/bootstrap_white.css";
 
 interface NarrativeTextProps {
   text: string;
@@ -9,7 +11,21 @@ interface NarrativeTextProps {
 const NarrativeText = ({ text }: NarrativeTextProps) => {
   return (
     <div className="NarrativeText">
-      <ReactMarkdown>{text}</ReactMarkdown>
+      <ReactMarkdown
+        children={text}
+        // turn this into tooltips
+        components={{
+          a: (props: any) => (
+            <Tooltip
+              placement="top"
+              overlay={props.href}
+              arrowContent={<div className="rc-tooltip-arrow-inner"></div>}
+            >
+              <a href="#">{props.children}</a>
+            </Tooltip>
+          ),
+        }}
+      />
     </div>
   );
 };
