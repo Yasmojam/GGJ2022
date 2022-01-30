09505af4b083cfa2eeb964e553ff816efebad7ed
Noel <14931745+FireRedNinja@users.noreply.github.com>
2022-01-30 16:36:52 +0000

    added fixes to display images more nicely (#26)
    * added fixes to display images more nicely
    
    * fix merge conflicts
    
    * updated files

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,8 @@
 import React, {useContext, useState} from "react";
 import { GameContext } from "./context/GameContext";
-import "./App.css";
+import "./App.scss";
 import GameComponent from "./components/GameComponent";
 
-
-function App() {
-  const gameState = useContext(GameContext);
-  const worldImgLocation = `./art/backgrounds/${gameState.backgroundImage}.png`;
-  const animalImgLocation = `./art/animals/${gameState.currentPassage?.name}.png`;
-
-  const options = gameState.currentPassage?.links ?? [];
-
-  return (
-      <div className="App">
-        {gameState.currentPassage?.pid != gameState.startNode && <ReactAudioPlayer src="sound/music/godcomplex.mp3" autoPlay loop volume={0.015} />}
-        <ReactAudioPlayer src={"sound/voiceover/english/"+ gameState.currentPassage?.name + ".m4a"} autoPlay volume={1.0} />
-
-        <div className="night">
-          {[...Array(30).keys()].map(_ =>
-            <div className="shooting_star"></div>
-          )}
-        </div>
-        
-        <div className="images">
-          <div className="world">
-            <BackgroundImage
-              src={worldImgLocation}
-            />
-          </div>
-
-          <div className="animal">
-            <BackgroundImage
-              src={animalImgLocation}
-            />
-          </div>
-        </div>
 
 function App() {
 
