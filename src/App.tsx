2caa66d4822d55630d12fc6f0aa87b2e69f62508
Noel <14931745+FireRedNinja@users.noreply.github.com>
2022-01-30 16:21:23 +0000

    fix merge conflicts

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,8 @@
 import React, {useContext, useState} from "react";
 import { GameContext } from "./context/GameContext";
-import "./App.css";
+import "./App.scss";
 import GameComponent from "./components/GameComponent";
 
-import files from './assets/files.json';
-
-function App() {
-  const gameState = useContext(GameContext);
-  const worldImgName = `${gameState.backgroundImage}.png`;
-  const worldImgLocation = `./art/backgrounds/${worldImgName}`;
-  const animalImgName = `${gameState.currentPassage?.name}.png`;
-  const animalImgLocation = `./art/animals/${animalImgName}`;
-
-  const options = gameState.currentPassage?.links ?? [];
-
-  return (
-      <div className="App">
-        {gameState.currentPassage?.pid != gameState.startNode && <ReactAudioPlayer src="sound/music/godcomplex.mp3" autoPlay loop volume={0.015} />}
-        <ReactAudioPlayer src={"sound/voiceover/english/"+ gameState.currentPassage?.name + ".m4a"} autoPlay volume={1.0} />
-
-        <div className="night">
-          {[...Array(30).keys()].map(_ =>
-            <div className="shooting_star"></div>
-          )}
-        </div>
-        
-        <div className="images">
-          <div className="world">
-            {
-              files.backgrounds.includes(worldImgName) && 
-              <BackgroundImage
-                src={worldImgLocation}
-                height={500}
-              />
-            }
-          </div>
-
-          <div className="animal">
-            {
-              files.animals.includes(animalImgName) &&
-              <BackgroundImage
-                src={animalImgLocation}
-                height={100}
-              />
-            }
-          </div>
-        </div>
 
 function App() {
 
--- src/components/GameComponent.tsx
+++ src/components/GameComponent.tsx
@@ -1,51 +1,72 @@
-import React, {useContext} from "react";
+import React, { useContext } from "react";
 import { GameContext } from "../context/GameContext";
 import Link from "./Link";
 import NarrativeText from "./NarrativeText";
 import BackgroundImage from "./BackgroundImage";
-import { AnimateOnChange } from 'react-animation';
-
-import LocalisationManager from "./localisationModel/localisation";
-
-import binary from "./assets/art/backgrounds/single.png";
+import { AnimateOnChange } from "react-animation";
+import files from '../assets/files.json';
+import './GameComponent.scss';
 import ReactAudioPlayer from "react-audio-player";
 
 export default function GameComponent() {
-
-	const gameState = useContext(GameContext); 
+  const gameState = useContext(GameContext);
   const options = gameState.currentPassage?.links ?? [];
+  const worldImgName = `${gameState.backgroundImage}.png`;
+  const worldImgLocation = `./art/backgrounds/${worldImgName}`;
+  const animalImgName = `${gameState.currentPassage?.name}.png`;
+  const animalImgLocation = `./art/animals/${animalImgName}`;
 
-	return (
-		<div>
-		{gameState.currentPassage.pid != gameState.startNode && <ReactAudioPlayer src="sound/music/godcomplex.mp3" autoPlay loop volume={0.015} />}
-		<ReactAudioPlayer src={"sound/voiceover/"+ gameState.currentPassage?.name + ".m4a"} autoPlay volume={1.0} />
+  return (
+    <div>
+      {gameState.currentPassage.pid != gameState.startNode && (
+        <ReactAudioPlayer
+          src="sound/music/godcomplex.mp3"
+          autoPlay
+          loop
+          volume={0.015}
+        />
+      )}
+      <ReactAudioPlayer
+        src={"sound/voiceover/" + gameState.currentPassage?.name + ".m4a"}
+        autoPlay
+        volume={1.0}
+      />
 
-		<div className="night">
-			{[...Array(30).keys()].map(_ =>
-				<div className="shooting_star"></div>
-			)}
-		</div>
+      <div className="night">
+        {[...Array(30).keys()].map((_) => (
+          <div className="shooting_star"></div>
+        ))}
+      </div>
 
-		<BackgroundImage
-			src={`./art/backgrounds/${gameState.backgroundImage}.png`}
-		/>
+      <div className="images">
+        <div className="world">
+          {files.backgrounds.includes(worldImgName) && (
+            <BackgroundImage src={worldImgLocation} height={500} />
+          )}
+        </div>
 
-		<AnimateOnChange
-				animationIn="fadeIn"
-				animationOut="fadeOut"
-				durationOut={500}
-		>
-			<NarrativeText text={gameState.currentPassage?.text} />
+        <div className="animal">
+          {files.animals.includes(animalImgName) && (
+            <BackgroundImage src={animalImgLocation} height={100} />
+          )}
+        </div>
+      </div>
 
-			<div className="LinksContainer">
-				{options.map((option:Link, index) => {
-					return(
-						<Link text={option.name} nextPassageId={option.pid} key={index} />
-					)
-				})}
-			</div>
-		</AnimateOnChange>
-		</div>
+      <AnimateOnChange
+        animationIn="fadeIn"
+        animationOut="fadeOut"
+        durationOut={500}
+      >
+        <NarrativeText text={gameState.currentPassage?.text} />
 
-	)
+        <div className="LinksContainer">
+          {options.map((option: Link, index) => {
+            return (
+              <Link text={option.name} nextPassageId={option.pid} key={index} />
+            );
+          })}
+        </div>
+      </AnimateOnChange>
+    </div>
+  );
 }
