738f6753fc3af83053622b7926fdb9326d37b982
Youssef Moawad <youssef_programming@icloud.com>
2022-01-30 12:18:14 +0000

    Translates to Russian and Arabic - still needs to actually save them

--- src/localisationModel/localisation.ts
+++ src/localisationModel/localisation.ts
@@ -5,6 +5,23 @@
 import StoryManager from "../twison/StoryManager";
 import { v4 as uuidv4 } from 'uuid';
 
+const translate = async (text: string, targetLanguage: string) => {
+	const API_KEY = "AIzaSyBa00-rEQeqhkyOB-WGC3VBm5GFp3xCKlA"; // baby don't hurt me, baby don't hurt me, no more
+
+	let url = `https://translation.googleapis.com/language/translate/v2?key=${API_KEY}`;
+	url += '&q=' + encodeURI(text);
+	url += `&target=${targetLanguage}`;
+
+	const res = await fetch(url, { 
+		method: 'GET',
+		headers: {
+			"Content-Type": "application/json",
+			Accept: "application/json"
+		}
+	})
+	return (await res.json()).data.translations[0].translatedText;
+}
+
 enum Locale {
 	English = "en",
 	Russian = "ru",
@@ -43,15 +60,17 @@ export default class LocalisationManager {
 
 		this.generalisedLocaleStory = {...storyManager.story};
 		
-		this.parseEnglishStoryToTextTokens();
+		this.parseEnglishStoryToTextTokens().then(() => {
+			console.log(JSON.stringify(this.generalisedLocaleStory));
+			console.log(JSON.stringify(this.localisationTokens));
+			console.log(JSON.stringify(this.textTokens));	
+		});
 
-		// console.log(JSON.stringify(this.generalisedLocaleStory));
-		// console.log(JSON.stringify(this.localisationTokens));
-		// console.log(JSON.stringify(this.textTokens));	
+		// console.log(translate("hello", Locale.Arabic));
 	}
 
 	// Uses generalisedLocaleStory as a workspace to parse the English story into text tokens. Changes generalisedLocaleStory in place.
-	parseEnglishStoryToTextTokens() {
+	async parseEnglishStoryToTextTokens() {
 		for(const passage of this.generalisedLocaleStory.passages) {
 			const textTokenID = uuidv4();
 			const textToken: TextToken = {
@@ -60,32 +79,70 @@ export default class LocalisationManager {
 			};
 
 			// Add the text of the passage to the text token.
-			const textLocalisationToken: LocalisationToken = {
+			const englishLocalisationToken: LocalisationToken = {
 				id: uuidv4(),
 				locale: Locale.English,
 				text: passage.text,
 			};
+
+			// Translate passage to Arabic and add to the text token.
+			const arabicLocalisationToken: LocalisationToken = {
+				id: uuidv4(),
+				locale: Locale.Arabic,
+				text: await translate(passage.text, Locale.Arabic),
+			};
+
+			// Translate passage to Russian and add to the text token.
+			const russianLocalisationToken: LocalisationToken = {
+				id: uuidv4(),
+				locale: Locale.Russian,
+				text: await translate(passage.text, Locale.Russian),
+			};
+
 			passage.text = textTokenID;
-			textToken.localisationTokens.push(textLocalisationToken);
-			this.localisationTokens.push(textLocalisationToken);
+			textToken.localisationTokens.push(englishLocalisationToken);
+			textToken.localisationTokens.push(arabicLocalisationToken);
+			textToken.localisationTokens.push(russianLocalisationToken);
+			this.localisationTokens.push(englishLocalisationToken);
+			this.localisationTokens.push(arabicLocalisationToken);
+			this.localisationTokens.push(russianLocalisationToken);
 			this.textTokens.push(textToken);
 		
 			// Add the links to the text token.
-			passage.links?.forEach((link: Link) => {
+			passage.links?.forEach(async (link: Link) => {
 				const linkTokenID = uuidv4();
 				const linkToken: TextToken = {
 					id: linkTokenID,
 					localisationTokens: [],
 				}
 				
-				const linkLocalisationToken: LocalisationToken = {
+				const englishLinkLocalisationToken: LocalisationToken = {
 					id: uuidv4(),
 					locale: Locale.English,
 					text: link.name,
 				}
+
+				// Translate link to Arabic and add to the text token.
+				const arabicLinkLocalisationToken: LocalisationToken = {
+					id: uuidv4(),
+					locale: Locale.Arabic,
+					text: await translate(link.name, Locale.Arabic),
+				}
+				
+				// Translate link to Russian and add to the text token.
+				const russianLinkLocalisationToken: LocalisationToken = {
+					id: uuidv4(),
+					locale: Locale.Russian,
+					text: await translate(link.name, Locale.Russian),
+				}
+
 				link.name = linkTokenID;
-				linkToken.localisationTokens.push(linkLocalisationToken);
-				this.localisationTokens.push(linkLocalisationToken);
+				linkToken.localisationTokens.push(englishLinkLocalisationToken);
+				linkToken.localisationTokens.push(arabicLinkLocalisationToken);
+				linkToken.localisationTokens.push(russianLinkLocalisationToken);
+				this.localisationTokens.push(englishLinkLocalisationToken);
+				this.localisationTokens.push(arabicLinkLocalisationToken);
+				this.localisationTokens.push(russianLinkLocalisationToken);
 				this.textTokens.push(linkToken);
 			});
 		}
